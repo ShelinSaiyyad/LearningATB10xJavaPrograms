4bab3ab609f2f5d52ec24d00d7de2ca637e59ffe
ssaiyyad <shelinsaiyyad3@gmail.com>
2025-01-10 16:30:05 +0530

    Hello..

---
+++ src/ex_01_Java_Basics/Lab005_Keywords.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab005_Keywords {
+
+    public static void main(String[] args) {
+
+        System.out.println("The words which are in orange color here are keywords.");
+    }
+}
---
+++ src/ex_02_Java_Basics/Using_printf_table_of_9.java
@@ -0,0 +1,22 @@
+package ex_02_Java_Basics;
+
+public class Using_printf_table_of_9 {
+
+    public static void main(String[] args) {
+
+        int number = 9;
+
+        System.out.println("Table of 9");
+        System.out.printf("9 x 1 = %d\n",number*1);
+        System.out.printf("9 x 1 = %d\n",number*2);
+        System.out.printf("9 x 1 = %d\n",number*3);
+        System.out.printf("9 x 1 = %d\n",number*4);
+        System.out.printf("9 x 1 = %d\n",number*5);
+        System.out.printf("9 x 1 = %d\n",number*6);
+        System.out.printf("9 x 1 = %d\n",number*7);
+        System.out.printf("9 x 1 = %d\n",number*8);
+        System.out.printf("9 x 1 = %d\n",number*9);
+        System.out.printf("9 x 1 = %d\n",number*10);
+
+    }
+}
